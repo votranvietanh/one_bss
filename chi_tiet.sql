cc9ea4655a45bf3f0b04449505e7354de194f78e
Viet Anh <93200025+votranvietanh@users.noreply.github.com>
2024-12-03 16:51:19 +0700

    refactor chi tiet bỏ các cột ID và tiền tháng

--- chi_tiet.sql
+++ chi_tiet.sql
@@ -127,3 +127,17 @@ x_onebss AS (
         a.tenchuquan
 )
 select * from x_onebss;
+
+
+
+
+
+
+---refactor CT
+select a.LOAIHINH_TB, a.MA_GD, a.HDTB_ID, a.THUEBAO_ID, a.MA_TB,c.ten_kh, a.MA_LOAIHD, a.TEN_LOAIHD, a.TEN_KIEULD, a.NGAY_YC, a.NGAY_HT, a.TEN_NV, a.TEN_DV, a.PBH, a.DONVI_TT_ID, a.TEN_PB_TTVT, a.NGAY_TT, a.NGAY_HD, a.SERI, a.SOSERI, a.TIEN, a.VAT, a.KM_LAPDAT, a.VAT_KM, a.HT_TRA,  a.KENHTHU, a.TRANGTHAI_TT, a.TENCHUQUAN, a.CHUQUAN_ID 
+from ttkdhcm_ktnv.baocao_doanhthu_dongtien_pktkh a
+left join css_hcm.db_thuebao b on a.thuebao_id = b.thuebao_id
+left join css_hcm.db_khachhang c on b.KHACHHANG_ID =c.KHACHHANG_ID
+where a.thang = 202411 and  a.khoanmuctt_id  in (1,2,3,4,9,17)
+        and a.tthd_id = 6 and a.dichvuvt_id in (1,4,7,8,9,10,11,12)
+;
