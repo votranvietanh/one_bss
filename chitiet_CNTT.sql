dca33c8d5febf0246c58e936ffc9fe66e173ba4a
Viet Anh <93200025+votranvietanh@users.noreply.github.com>
2025-08-04 15:23:54 +0700

    Update chitiet_CNTT.sql
    comment lại dòng 71 theo y/c kế toán lấy theo vb mới

--- chitiet_CNTT.sql
+++ chitiet_CNTT.sql
@@ -1,7 +1,8 @@
+
 select a.LOAITB_ID,a.LOAIHINH_TB,a.khoanmuctt_id, a.MA_GD, a.HDTB_ID, a.THUEBAO_ID, a.MA_TB,c.ten_kh, a.MA_LOAIHD, a.TEN_LOAIHD, a.TEN_KIEULD, a.NGAY_YC, a.NGAY_HT, a.TEN_NV, a.TEN_DV, a.PBH, a.DONVI_TT_ID, a.TEN_PB_TTVT, a.NGAY_TT, a.NGAY_HD, a.SERI, a.SOSERI, 
  a.TIEN, a.VAT,a.TIEN+a.VAT tong_ps, (CASE
             WHEN 
-                 (TO_NUMBER(TO_CHAR(NGAY_TT, 'YYYYMM')) = 202506  or ngay_tt is null )
+                 (TO_NUMBER(TO_CHAR(NGAY_TT, 'YYYYMM')) = 202507  or ngay_tt is null )
                 and nvl(chuquan_id,0) <> 264
                 and trangthai = 1
 
@@ -10,7 +11,7 @@ select a.LOAITB_ID,a.LOAIHINH_TB,a.khoanmuctt_id, a.MA_GD, a.HDTB_ID, a.THUEBAO_
         END) AS tien_thu_trong_thang,
         (CASE
             WHEN 
-                 (TO_NUMBER(TO_CHAR(NGAY_TT, 'YYYYMM')) = 202506  or ngay_tt is null )
+                 (TO_NUMBER(TO_CHAR(NGAY_TT, 'YYYYMM')) = 202507  or ngay_tt is null )
                 and nvl(chuquan_id,0) <> 264
                 and trangthai = 1
 
@@ -19,7 +20,7 @@ select a.LOAITB_ID,a.LOAIHINH_TB,a.khoanmuctt_id, a.MA_GD, a.HDTB_ID, a.THUEBAO_
         END) AS vat_thu_trong_thang,
         (CASE
             WHEN 
-                (TO_NUMBER(TO_CHAR(NGAY_TT, 'YYYYMM')) = 202506  or ngay_tt is null )
+                (TO_NUMBER(TO_CHAR(NGAY_TT, 'YYYYMM')) = 202507  or ngay_tt is null )
                 and nvl(chuquan_id,0) <> 264
                 and trangthai = 1
 
@@ -28,20 +29,20 @@ select a.LOAITB_ID,a.LOAIHINH_TB,a.khoanmuctt_id, a.MA_GD, a.HDTB_ID, a.THUEBAO_
         END) AS tong_thu_trong_thang
         ,
         (CASE
-            WHEN (TO_NUMBER(TO_CHAR(ngay_tt, 'YYYYMM')) <> 202506 and trangthai= 1)
+            WHEN (TO_NUMBER(TO_CHAR(ngay_tt, 'YYYYMM')) <> 202507 and trangthai= 1)
 
             THEN tien + km_lapdat
             ELSE 0
         END) AS tien_thu_thang_truoc,
         (CASE
-            WHEN (TO_NUMBER(TO_CHAR(ngay_tt, 'YYYYMM')) <> 202506 and trangthai= 1)
+            WHEN (TO_NUMBER(TO_CHAR(ngay_tt, 'YYYYMM')) <> 202507 and trangthai= 1)
 
             THEN vat + vat_km
             ELSE 0
         END) AS vat_thu_thang_truoc,
         (CASE
             WHEN 
-                (TO_NUMBER(TO_CHAR(ngay_tt, 'YYYYMM')) <> 202506  and trangthai= 1)
+                (TO_NUMBER(TO_CHAR(ngay_tt, 'YYYYMM')) <> 202507  and trangthai= 1)
 
             THEN vat + vat_km + tien + km_lapdat
             ELSE 0
@@ -66,7 +67,9 @@ a.KM_LAPDAT, a.VAT_KM, a.HT_TRA,  a.KENHTHU, a.TRANGTHAI_TT, a.TENCHUQUAN, a.CHU
 from ttkdhcm_ktnv.baocao_doanhthu_dongtien_pktkh a
 left join css_hcm.db_thuebao b on a.thuebao_id = b.thuebao_id
 left join css_hcm.db_khachhang c on b.KHACHHANG_ID =c.KHACHHANG_ID
-where a.thang = 202506 and       a.tthd_id = 6 
-         AND a.loaitb_id not in (35,80,148,350,2116,147,39,290,175,122,319,208,288,373) -- dk chi Huong+319,208: elearning va eticket
+where a.thang = 202507 and    a.tthd_id = 6 
+--         AND a.loaitb_id not in (35,80,148,350,2116,147,39,290,175,122,319,208,288,373) -- dk chi Huong+319,208: elearning va eticket
          AND a.dichvuvt_id not in (1,4,7,8,9,10,11,12)
-         AND a.khoanmuctt_id in (1,5,52,142) and a.ma_tb in(select ma_tb from vietanhvh.onebss_202506)
+         AND a.khoanmuctt_id in (1,5,52,142) and a.ma_tb in(select ma_tb from vietanhvh.onebss_202507)
+         ;
+
